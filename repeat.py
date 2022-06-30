61a1af8f30d13ab64834cc95bc252e785ead42e6
Aleksander <224287@edu.p.lodz.pl>
2022-06-30 20:55:16 +0200

    async

--- repeat.py
+++ repeat.py
@@ -2,7 +2,8 @@ import argparse
 import configparser
 import datetime
 import time
-import requests as req
+import aiohttp
+import asyncio
 import os
 from threading import Thread
 from collections import defaultdict
@@ -22,7 +23,8 @@ def main():
     parser.add_argument('-r', '--ratio', type=int, default=0, help='Ratio of request sending per minute, 0 is max, '
                                                                    '-r 1 means 1 request per minute')
     parser.add_argument('-f', '--file', type=str, help='Path to the file to send')
-    parser.add_argument('-rd', '--random', type=str, default=0, help='Sleep is 60/ratio, random defines the percent of this number to add/delete to this equation')
+    parser.add_argument('-rd', '--random', type=str, default=0,
+                        help='Sleep is 60/ratio, random defines the percent of this number to add/delete to this equation')
     parser.add_argument('-t', '--threads', type=int, default=1, help='Number of threads used to execute request '
                                                                      'repetition. default = 1, system max = 0')
     parser.add_argument('-c', '--config', type=str, help='Path to the config file in format:\n'
@@ -53,6 +55,8 @@ def main():
         file = config["File"]
         threads = int(config["Threads"])
         random_c = int(config["Random"])
+        if threads == 0:
+            threads = os.cpu_count()
     elif args.link is None:
         raise AssertionError('Link must be provided')
     elif args.file is None:
@@ -78,30 +82,41 @@ def main():
             threads = args.threads
 
     # noinspection PyUnboundLocalVariable
-    for x in range(1, threads):
-        t = Thread(target=request_sender, args=(x, start_date, end_date, ratio, link, file, random_c))
+    for x in range(0, threads):
+        t = Thread(target=asyncio.run, args=(request_sender(x, start_date, end_date, ratio, link, file, random_c),))
         t.start()
-    t = Thread(target=request_sender, args=(0, start_date, end_date, ratio, link, file, random_c, True))
+
+    t = Thread(target=print_stats, args=(start_date, end_date))
     t.start()
 
 
-def request_sender(name, start, stop, ratio, link, file, random_c, stat=False):
+def print_stats(start, stop):
     real_start = datetime.datetime.now()
     while datetime.datetime.now() < stop:
         if start < datetime.datetime.now():
+            time.sleep(10)
+            t = (datetime.datetime.now() - real_start).total_seconds()
+            s = sum(stats.values())
+            print(f'Requests sent: {s}, time elapsed: {str(t)}, | {s / t} r/s')
+        else:
+            time.sleep(10)
+            real_start = datetime.datetime.now()
+
+
+async def request_sender(name, start, stop, ratio, link, file, random_c):
+    while datetime.datetime.now() < stop:
+        if start < datetime.datetime.now():
             stats[name] += 1
-            req.post(link, files={'pdf': open(file, 'rb')})
-            if stat:
-                t = (datetime.datetime.now() - real_start).total_seconds()
-                s = sum(stats.values())
-                print(f'Requests sent: {s}, time elapsed: {str(t)}, | {s/t} r/s\r', end="")
+            async with aiohttp.ClientSession() as session:
+                async with session.post(link, data={'pdf': open(file, 'rb')}) as xd:
+                    pass
+
             if ratio != 0:
                 t = 60 / ratio
-                t += random.randint(-random_c, random_c)/100 * t
+                t += random.randint(-random_c, random_c) / 100 * t
                 time.sleep(t)
         else:
             time.sleep(60)
-            real_start = datetime.datetime.now()
 
 
 if __name__ == '__main__':
